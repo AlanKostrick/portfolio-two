1c39e2b9372c01bb1c26bac66de79f03e3c785b0
YourGitHubName <alan@wecancodeit.org>
2018-07-08 17:08:22 -0400

    part deux

---
+++ js/app.js
@@ -0,0 +1,167 @@
+//PROGRAMMER
+// Get the modal
+var programmerModal = document.getElementById('programmerModal');
+
+// Get the button that opens the modal
+var btnProgrammer = document.getElementById("programmer");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close programmer")[0];
+
+// When the user clicks the button, open the modal 
+btnProgrammer.onclick = function() {
+    programmerModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    programmerModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == programmerModal) {
+        programmerModal.style.display = "none";
+    }
+}
+
+
+
+//INSTRUCTOR
+// Get the modal
+var instructorModal = document.getElementById('instructorModal');
+
+// Get the button that opens the modal
+var btnInstructor = document.getElementById("instructor");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close instructor")[0];
+
+// When the user clicks the button, open the modal 
+btnInstructor.onclick = function() {
+    instructorModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    instructorModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == instructorModal) {
+        instructorModal.style.display = "none";
+    }
+}
+
+
+//SKATEBOARDER
+// Get the modal
+var skateboarderModal = document.getElementById('skateboarderModal');
+
+// Get the button that opens the modal
+var btnSkateboarder = document.getElementById("skateboarder");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close skateboarder")[0];
+
+// When the user clicks the button, open the modal 
+btnSkateboarder.onclick = function() {
+    skateboarderModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    skateboarderModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == skateboarderModal) {
+        skateboarderModal.style.display = "none";
+    }
+}
+
+
+//DAD
+// Get the modal
+var fatherModal = document.getElementById('fatherModal');
+
+// Get the button that opens the modal
+var btnFather = document.getElementById("father");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close father")[0];
+
+// When the user clicks the button, open the modal 
+btnFather.onclick = function() {
+    fatherModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    fatherModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == fatherModal) {
+        fatherModal.style.display = "none";
+    }
+}
+
+
+//HUSBAND
+// Get the modal
+var husbandModal = document.getElementById('husbandModal');
+
+// Get the button that opens the modal
+var btnHusband = document.getElementById("husband");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close husband")[0];
+
+// When the user clicks the button, open the modal 
+btnHusband.onclick = function() {
+    husbandModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    husbandModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == husbandModal) {
+        husbandModal.style.display = "none";
+    }
+}
+
+
+//MUSICIAN
+// Get the modal
+var musicianModal = document.getElementById('musicianModal');
+
+// Get the button that opens the modal
+var btnMusician = document.getElementById("musician");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close musician")[0];
+
+// When the user clicks the button, open the modal 
+btnMusician.onclick = function() {
+    musicianModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    musicianModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == musicianModal) {
+        musicianModal.style.display = "none";
+    }
+}
